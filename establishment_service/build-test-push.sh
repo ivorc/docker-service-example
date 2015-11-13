ce95352169554257158088a806526ef626df8111
Ivor Caldwell <ivor.caldwell@infinityworks.com>
2015-11-13 16:46:25 +0000

    No sudo when running docker as rancheros doesn't have/need it

--- establishment_service/build-test-push.sh
+++ establishment_service/build-test-push.sh
@@ -5,12 +5,12 @@ set -x
 result=0 # OK
 
 if [ $result == 0 ] ; then
-    sudo docker build -t ivorcaldwell/python-service-example .
+    docker build -t ivorcaldwell/python-service-example .
     result=$?
 fi
 
 if [ $result == 0 ] ; then
-    sudo docker run --rm ivorcaldwell/python-service-example bash -c 'PYTHONPATH=.:$PYTHONPATH py.test'
+    docker run --rm ivorcaldwell/python-service-example bash -c 'PYTHONPATH=.:$PYTHONPATH py.test'
     result=$?
 fi
 
